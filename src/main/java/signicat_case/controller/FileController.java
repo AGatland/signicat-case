eb4ed2a33ab70280c449e9f58a68381005fef28a
Alexander Gatland <alexandergatland@gmail.com>
2024-06-25 11:49:50 +0200

    Set up service for FileController closes #4

--- src/main/java/signicat_case/controller/FileController.java
+++ src/main/java/signicat_case/controller/FileController.java
@@ -1,16 +1,22 @@
 package signicat_case.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import signicat_case.service.FileService;
+
 @RestController
 @RequestMapping("file")
 public class FileController {
     
+    @Autowired
+    FileService fileService;
+
     @GetMapping("/test")
     public ResponseEntity<?> testEndpoint() {
-        return ResponseEntity.ok("Working");
+        return ResponseEntity.ok(fileService.testConnection());
     }
 }
---
+++ src/main/java/signicat_case/service/FileService.java
@@ -0,0 +1,12 @@
+package signicat_case.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileService {
+
+    public String testConnection() {
+        return "Working";
+    }
+    
+}
